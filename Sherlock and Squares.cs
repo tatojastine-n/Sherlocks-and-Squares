bc5deca971d94b5080714cbdb61217b0c1914255
tatojastine-n <jajanicoletato@gmail.com>
2025-10-31 14:50:05 +0800

    Sherlock and Squares.cs

---
+++ Sherlock and Squares.cs
@@ -0,0 +1,72 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'squares' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER a
+     *  2. INTEGER b
+     */
+
+    public static int squares(int a, int b)
+    {
+        long start = 1;
+        while (start * start < a) start++;
+
+        long end = 0;
+        while (end * end <= b) end++;
+        end--;
+
+        if (start <= end)
+        {
+            return (int)(end - start + 1);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int a = Convert.ToInt32(firstMultipleInput[0]);
+
+            int b = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.squares(a, b);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
